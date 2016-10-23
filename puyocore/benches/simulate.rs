d29ac02a8e9ef7dfc71cfa810c2c98f2fa005e3d
mayah <mayah@mayah.jp>
2016-10-24 00:31:25 +0900

    Add BitField::simulate_fast bench

--- puyocore/benches/simulate.rs
+++ puyocore/benches/simulate.rs
@@ -27,3 +27,25 @@ fn simulate_19rensa(b: &mut Bencher) {
         test::black_box(bf.clone().simulate())
     })
 }
+
+#[bench]
+fn simulate_fast_19rensa(b: &mut Bencher) {
+    let bf = BitField::from_str(concat!(
+        ".G.BRG",
+        "GBRRYR",
+        "RRYYBY",
+        "RGYRBR",
+        "YGYRBY",
+        "YGBGYR",
+        "GRBGYR",
+        "BRBYBY",
+        "RYYBYY",
+        "BRBYBR",
+        "BGBYRR",
+        "YGBGBG",
+        "RBGBGG"));
+
+    b.iter(|| {
+        test::black_box(bf.clone().simulate_fast())
+    })
+}
