f42d7ab0152c8c410355c5bd7f589f2be1b82739
mayah <mayah@mayah.jp>
2016-11-03 18:28:12 +0900

    Add KeySet::parse_keysetseq
    parse_keysetseq parses keyset sequence string and make a Vec for that.

--- puyocore/src/control/key_set.rs
+++ puyocore/src/control/key_set.rs
@@ -1,5 +1,6 @@
 use control::Key;
 
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct KeySet {
     keys: usize,  // bit flags
 }
@@ -11,6 +12,20 @@ impl KeySet {
         }
     }
 
+    pub fn from_key(k: Key) -> KeySet {
+        let mut ks = KeySet::new();
+        ks.set_key(k);
+        ks
+    }
+
+    pub fn from_keys(keys: &[Key]) -> KeySet {
+        let mut ks = KeySet::new();
+        for k in keys {
+            ks.set_key(*k);
+        }
+        ks
+    }
+
     pub fn set_key(&mut self, k: Key) {
         self.keys = self.keys | (1 << (k as usize))
     }
@@ -20,9 +35,21 @@ impl KeySet {
     }
 }
 
+pub fn parse_keysetseq(s: &str) -> Result<Vec<KeySet>, String> {
+    let mut keysetseq = Vec::new();
+    for x in s.split(',') {
+        let mut ks = KeySet::new();
+        for c in x.chars() {
+            ks.set_key(try!(Key::parse_char(c)))
+        }
+        keysetseq.push(ks);
+    }
+    Ok(keysetseq)
+}
+
 #[cfg(test)]
 mod tests {
-    use super::KeySet;
+    use super::*;
     use control::Key;
 
     #[test]
@@ -45,5 +72,34 @@ mod tests {
         assert!(!ks.has_key(Key::RightTurn));
         assert!(!ks.has_key(Key::LeftTurn));
         assert!(!ks.has_key(Key::Start));
+    }
+
+    #[test]
+    fn test_parse_keysetseq_1() {
+        let expected: &[KeySet] = &[
+            KeySet::from_key(Key::Right),
+            KeySet::from_key(Key::Left),
+            KeySet::from_key(Key::Down),
+            KeySet::from_key(Key::Up),
+            KeySet::from_key(Key::RightTurn),
+            KeySet::from_key(Key::LeftTurn),
+            KeySet::from_key(Key::Start),
+        ];
+
+        assert_eq!(expected, parse_keysetseq(">,<,v,^,A,B,S").unwrap().as_slice());
+    }
+
+    #[test]
+    fn test_parse_keysetseq_2() {
+        let expected: &[KeySet] = &[
+            KeySet::from_keys(&[Key::Right, Key::RightTurn]),
+            KeySet::from_keys(&[Key::RightTurn, Key::Right]),
+            KeySet::from_keys(&[Key::Left, Key::LeftTurn]),
+            KeySet::from_keys(&[Key::LeftTurn, Key::Left]),
+            KeySet::from_keys(&[Key::Down, Key::RightTurn]),
+            KeySet::from_keys(&[Key::Down, Key::LeftTurn]),
+        ];
+
+        assert_eq!(expected, parse_keysetseq(">A,>A,<B,<B,vA,vB").unwrap().as_slice());
     }
 }
