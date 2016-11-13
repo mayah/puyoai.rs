6c865ff16a142ea63dd58db6e0fd2d229665c00b
mayah <mayah@mayah.jp>
2016-11-13 11:07:55 +0900

    :white_check_mark: Add tests for PuyoSet

--- puyocore/src/probability/puyo_set.rs
+++ puyocore/src/probability/puyo_set.rs
@@ -18,4 +18,40 @@ impl PuyoSet {
     pub fn empty() -> PuyoSet {
         PuyoSet::new(0, 0, 0, 0)
     }
+
+    pub fn red(&self) -> usize {
+        self.red
+    }
+
+    pub fn blue(&self) -> usize {
+        self.blue
+    }
+
+    pub fn yellow(&self) -> usize {
+        self.yellow
+    }
+
+    pub fn green(&self) -> usize {
+        self.green
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic() {
+        let ps = PuyoSet::empty();
+        assert_eq!(ps.red(), 0);
+        assert_eq!(ps.blue(), 0);
+        assert_eq!(ps.yellow(), 0);
+        assert_eq!(ps.green(), 0);
+
+        let ps = PuyoSet::new(1, 2, 3, 4);
+        assert_eq!(ps.red(), 1);
+        assert_eq!(ps.blue(), 2);
+        assert_eq!(ps.yellow(), 3);
+        assert_eq!(ps.green(), 4);
+    }
 }
