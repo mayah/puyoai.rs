98ed4b956e462c7ebed6f44190531e0e20360733
mayah <mayah@mayah.jp>
2016-11-29 23:07:20 +0900

    Port CoreField::frames_to_drop_next

--- puyoai-core/src/field/core_field.rs
+++ puyoai-core/src/field/core_field.rs
@@ -2,6 +2,8 @@ use color::PuyoColor;
 use column_puyo_list::ColumnPuyoList;
 use decision::Decision;
 use field::{self, BitField, FieldHeight, FieldIsEmpty};
+use frame;
+
 use std;
 
 #[derive(Clone, Debug, PartialEq)]
@@ -124,6 +126,66 @@ impl CoreField {
 
         true
     }
+
+    pub fn frames_to_drop_next(&self, decision: &Decision) -> usize {
+        // TODO(mayah): This calculation should be more accurate. We need to compare this with
+        // actual AC puyo2 and duel server algorithm. These must be much the same.
+
+        // TODO(mayah): When "kabegoe" happens, we need more frames.
+        const KABEGOE_PENALTY: usize = 6;
+
+        let x1 = decision.axis_x();
+        let x2 = decision.child_x();
+
+        let mut drop_frames = frame::FRAMES_TO_MOVE_HORIZONTALLY[(3 - x1 as isize).abs() as usize];
+
+        if decision.rot() == 0 {
+            let drop_height = field::HEIGHT as isize - self.height(x1) as isize;
+            if drop_height <= 0 {
+                // TODO(mayah): We need to add penalty here. How much penalty is necessary?
+                drop_frames += KABEGOE_PENALTY + frame::FRAMES_GROUNDING;
+            } else {
+                drop_frames += frame::FRAMES_TO_DROP_FAST[drop_height as usize] + frame::FRAMES_GROUNDING;
+            }
+        } else if decision.rot() == 2 {
+            let mut drop_height = (field::HEIGHT as isize) - (self.height(x1) as isize) - 1;
+            // TODO: If puyo lines are high enough, rotation might take time. We should measure this later.
+            // It looks we need 3 frames to waiting that each rotation has completed.
+            if drop_height < 6 {
+                drop_height = 6;
+            }
+
+            drop_frames += frame::FRAMES_TO_DROP_FAST[drop_height as usize] + frame::FRAMES_GROUNDING;
+        } else {
+            if self.height(x1) == self.height(x2) {
+                let drop_height = field::HEIGHT as isize - self.height(x1) as isize;
+                if drop_height <= 0 {
+                    drop_frames += KABEGOE_PENALTY + frame::FRAMES_GROUNDING;
+                } else if drop_height < 3 {
+                    drop_frames += frame::FRAMES_TO_DROP_FAST[3] + frame::FRAMES_GROUNDING;
+                } else {
+                    drop_frames += frame::FRAMES_TO_DROP_FAST[drop_height as usize] + frame::FRAMES_GROUNDING;
+                }
+            } else {
+                let min_height = std::cmp::min(self.height(x1), self.height(x2));
+                let max_height = std::cmp::max(self.height(x1), self.height(x2));
+                let diff_height = max_height - min_height;
+                let drop_height = field::HEIGHT as isize - max_height as isize;
+                if drop_height <= 0 {
+                    drop_frames += KABEGOE_PENALTY;
+                } else if drop_height < 3 {
+                    drop_frames += frame::FRAMES_TO_DROP_FAST[3];
+                } else {
+                    drop_frames += frame::FRAMES_TO_DROP_FAST[drop_height as usize];
+                }
+                drop_frames += frame::FRAMES_GROUNDING;
+                drop_frames += frame::FRAMES_TO_DROP[diff_height as usize];
+                drop_frames += frame::FRAMES_GROUNDING;
+            }
+        }
+
+        drop_frames
+    }
 }
 
 impl FieldHeight for CoreField {
@@ -145,6 +207,7 @@ mod tests {
     use column_puyo_list::ColumnPuyoList;
     use decision::Decision;
     use field;
+    use frame;
 
     #[test]
     fn test_constructor() {
@@ -284,5 +347,66 @@ mod tests {
         assert!(cf.is_chigiri_decision(&Decision::new(3, 1)));
         assert!(!cf.is_chigiri_decision(&Decision::new(3, 2)));
         assert!(cf.is_chigiri_decision(&Decision::new(3, 3)));
+    }
+
+    #[test]
+    fn test_frames_to_drop_next_without_chigiri() {
+        // TODO(mayah): We have to confirm this.
+        let cf = CoreField::new();
+
+        assert_eq!(frame::FRAMES_TO_DROP_FAST[field::HEIGHT] + frame::FRAMES_GROUNDING,
+                   cf.frames_to_drop_next(&Decision::new(3, 0)));
+        assert_eq!(frame::FRAMES_TO_DROP_FAST[field::HEIGHT] + frame::FRAMES_GROUNDING,
+                   cf.frames_to_drop_next(&Decision::new(3, 1)));
+        assert_eq!(frame::FRAMES_TO_DROP_FAST[field::HEIGHT - 1] + frame::FRAMES_GROUNDING,
+                   cf.frames_to_drop_next(&Decision::new(3, 2)));
+        assert_eq!(frame::FRAMES_TO_DROP_FAST[field::HEIGHT] + frame::FRAMES_GROUNDING,
+                   cf.frames_to_drop_next(&Decision::new(3, 3)));
+        assert_eq!(frame::FRAMES_TO_MOVE_HORIZONTALLY[2] + frame::FRAMES_TO_DROP_FAST[field::HEIGHT] + frame::FRAMES_GROUNDING,
+                   cf.frames_to_drop_next(&Decision::new(1, 0)));
+    }
+
+    #[test]
+    fn test_frames_to_drop_next_with_chigiri() {
+        let cf = CoreField::from_str(concat!(
+            "..O...",
+            "..O...",
+            "..O...",
+            "..O...",
+        ));
+
+        let expected = frame::FRAMES_TO_DROP_FAST[field::HEIGHT - 4] + frame::FRAMES_GROUNDING +
+            frame::FRAMES_TO_DROP[4] + frame::FRAMES_GROUNDING;
+        assert_eq!(expected, cf.frames_to_drop_next(&Decision::new(3, 1)));
+    }
+
+    #[test]
+    fn test_frames_to_drop_next_on_13th_row() {
+        let cf = CoreField::from_str(concat!(
+            "OO.OOO", // 12
+            "OOOOOO",
+            "OOOOOO",
+            "OOOOOO",
+            "OOOOOO", // 8
+            "OOOOOO",
+            "OOOOOO",
+            "OOOOOO",
+            "OOOOOO", // 4
+            "OOOOOO",
+            "OOOOOO",
+            "OOOOOO",
+        ));
+
+        assert_eq!(11, cf.height(3));
+        assert_eq!(12, cf.height(4));
+
+        // We cannot put with Decision(4, 2).
+
+        assert_eq!(6 + frame::FRAMES_TO_MOVE_HORIZONTALLY[1] + frame::FRAMES_GROUNDING,
+                   cf.frames_to_drop_next(&Decision::new(4, 0)));
+        assert_eq!(6 + frame::FRAMES_TO_MOVE_HORIZONTALLY[1] + frame::FRAMES_GROUNDING,
+                   cf.frames_to_drop_next(&Decision::new(4, 1)));
+        assert_eq!(6 + frame::FRAMES_TO_MOVE_HORIZONTALLY[1] + frame::FRAMES_GROUNDING + frame::FRAMES_TO_DROP[1] + frame::FRAMES_GROUNDING,
+                   cf.frames_to_drop_next(&Decision::new(4, 3)));
     }
 }
