d24a461e1b53d1abeffa4e1c4834945443d5cbc6
mayah <mayah@mayah.jp>
2017-05-08 06:26:13 +0900

    Introduce PuyoController struct
    The struct will contain configuration. Currently it's empty struct.

--- puyoai-core/src/control/controller.rs
+++ puyoai-core/src/control/controller.rs
@@ -1,63 +1,74 @@
 use field::{FieldHeight, FieldIsEmpty};
 use decision::Decision;
 
-pub fn is_reachable<F: FieldHeight + FieldIsEmpty>(field: &F, decision: &Decision) -> bool {
-    debug_assert!(decision.is_valid());
-
-    const CHECKER: &'static [&'static [usize]] = &[
-        &[3, 2, 1, 0],
-        &[3, 2, 0],
-        &[3, 0],
-        &[3, 4, 0],
-        &[3, 4, 5, 0],
-        &[3, 4, 5, 6, 0],
-    ];
-
-    let mut checker_idx = decision.axis_x() - 1;
-    if decision.rot() == 1 && 3 <= decision.axis_x() {
-        checker_idx += 1;
-    } else if decision.rot() == 3 && decision.axis_x() <= 3 {
-        checker_idx -= 1;
-    }
-
-    let mut y_might_be_13 = field.height(2) >= 12 && field.height(4) >= 12;
-    let mut i = 1;
-
-    while CHECKER[checker_idx][i] != 0 {
-        let x = CHECKER[checker_idx][i];
-        if field.height(x) <= 11 {
-            y_might_be_13 = false;
-            i += 1;
-            continue;
-        }
-
-        if field.height(x) == 12 {
-            if y_might_be_13 {
-                i += 1;
-                continue;
-            }
-
-            if field.height(CHECKER[checker_idx][i - 1]) == 11 {
-                y_might_be_13 = true;
-                i += 1;
-                continue;
-            }
-            if i >= 2 && field.height(CHECKER[checker_idx][i - 2]) == 12 {
-                y_might_be_13 = true;
-                i += 1;
-                continue;
-            }
-        }
-
-        return false;
-    }
-
-    if decision.rot() == 2 && field.height(decision.axis_x()) >= 12 {
-        return false;
-    }
-
-    return true;
+pub struct PuyoController {
 }
+
+impl PuyoController {
+    pub fn new() -> PuyoController {
+        PuyoController {
+        }
+    }
+
+    pub fn is_reachable<F: FieldHeight + FieldIsEmpty>(&self, field: &F, decision: &Decision) -> bool {
+        debug_assert!(decision.is_valid());
+
+        const CHECKER: &'static [&'static [usize]] = &[
+            &[3, 2, 1, 0],
+            &[3, 2, 0],
+            &[3, 0],
+            &[3, 4, 0],
+            &[3, 4, 5, 0],
+            &[3, 4, 5, 6, 0],
+        ];
+
+        let mut checker_idx = decision.axis_x() - 1;
+        if decision.rot() == 1 && 3 <= decision.axis_x() {
+            checker_idx += 1;
+        } else if decision.rot() == 3 && decision.axis_x() <= 3 {
+            checker_idx -= 1;
+        }
+
+        let mut y_might_be_13 = field.height(2) >= 12 && field.height(4) >= 12;
+        let mut i = 1;
+
+        while CHECKER[checker_idx][i] != 0 {
+            let x = CHECKER[checker_idx][i];
+            if field.height(x) <= 11 {
+                y_might_be_13 = false;
+                i += 1;
+                continue;
+            }
+
+            if field.height(x) == 12 {
+                if y_might_be_13 {
+                    i += 1;
+                    continue;
+                }
+
+                if field.height(CHECKER[checker_idx][i - 1]) == 11 {
+                    y_might_be_13 = true;
+                    i += 1;
+                    continue;
+                }
+                if i >= 2 && field.height(CHECKER[checker_idx][i - 2]) == 12 {
+                    y_might_be_13 = true;
+                    i += 1;
+                    continue;
+                }
+            }
+
+            return false;
+        }
+
+        if decision.rot() == 2 && field.height(decision.axis_x()) >= 12 {
+            return false;
+        }
+
+        return true;
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -67,15 +78,17 @@ mod tests {
 
     #[test]
     fn test_is_reachable_empty_field() {
+        let pc = PuyoController::new();
         let f = FieldWithHeight::from_field(PuyoPlainField::new());
 
         for d in Decision::all_valid_decisions() {
-            assert!(is_reachable(&f, d));
+            assert!(pc.is_reachable(&f, d));
         }
     }
 
     #[test]
     fn test_is_reachable_upper_1() {
+        let pc = PuyoController::new();
         let f = FieldWithHeight::from_field(PuyoPlainField::from_str(concat!(
             "......", // 12
             "OOOOOO",
@@ -91,12 +104,13 @@ mod tests {
             "OOOOOO")));
 
         for d in Decision::all_valid_decisions() {
-            assert!(is_reachable(&f, d));
+            assert!(pc.is_reachable(&f, d));
         }
     }
 
     #[test]
     fn test_is_reachable_upper_2() {
+        let pc = PuyoController::new();
         let f = FieldWithHeight::from_field(PuyoPlainField::from_str(concat!(
             "......", // 12
             "......",
@@ -112,12 +126,13 @@ mod tests {
             "OOOOOO")));
 
         for d in Decision::all_valid_decisions() {
-            assert!(is_reachable(&f, d));
+            assert!(pc.is_reachable(&f, d));
         }
     }
 
     #[test]
     fn test_is_reachable_upper_3() {
+        let pc = PuyoController::new();
         let f = FieldWithHeight::from_field(PuyoPlainField::from_str(concat!(
             "O....O", // 12
             "OOOOOO",
@@ -138,12 +153,13 @@ mod tests {
         ];
 
         for d in Decision::all_valid_decisions() {
-            assert_eq!(is_reachable(&f, d), !unreachables.contains(d));
+            assert_eq!(pc.is_reachable(&f, d), !unreachables.contains(d));
         }
     }
 
     #[test]
     fn test_is_reachable_upper_4() {
+        let pc = PuyoController::new();
         let f = FieldWithHeight::from_field(PuyoPlainField::from_str(concat!(
             ".O..O.", // 12
             "OOOOOO",
@@ -164,12 +180,13 @@ mod tests {
         ];
 
         for d in Decision::all_valid_decisions() {
-            assert_eq!(is_reachable(&f, d), !unreachables.contains(d));
+            assert_eq!(pc.is_reachable(&f, d), !unreachables.contains(d));
         }
     }
 
     #[test]
     fn test_is_reachable_upper_5() {
+        let pc = PuyoController::new();
         let f = FieldWithHeight::from_field(PuyoPlainField::from_str(concat!(
             "...O..", // 12
             "OOOOOO",
@@ -189,12 +206,13 @@ mod tests {
         ];
 
         for d in Decision::all_valid_decisions() {
-            assert_eq!(is_reachable(&f, d), !unreachables.contains(d));
+            assert_eq!(pc.is_reachable(&f, d), !unreachables.contains(d));
         }
     }
 
     #[test]
     fn test_is_reachable_upper_6() {
+        let pc = PuyoController::new();
         let f = FieldWithHeight::from_field(PuyoPlainField::from_str(concat!(
             ".O.O..", // 12
             "OOOOOO",
@@ -215,12 +233,13 @@ mod tests {
         ];
 
         for d in Decision::all_valid_decisions() {
-            assert_eq!(is_reachable(&f, d), !unreachables.contains(d));
+            assert_eq!(pc.is_reachable(&f, d), !unreachables.contains(d));
         }
     }
 
     #[test]
     fn test_is_reachable_upper_7() {
+        let pc = PuyoController::new();
         let f = FieldWithHeight::from_field(PuyoPlainField::from_str(concat!(
             ".O.O..", // 12
             ".O.O..",
@@ -241,12 +260,13 @@ mod tests {
         ];
 
         for d in Decision::all_valid_decisions() {
-            assert_eq!(is_reachable(&f, d), !unreachables.contains(d));
+            assert_eq!(pc.is_reachable(&f, d), !unreachables.contains(d));
         }
     }
 
     #[test]
     fn test_is_reachable_upper_8() {
+        let pc = PuyoController::new();
         let f = FieldWithHeight::from_field(PuyoPlainField::from_str(concat!(
             "O...OO", // 13
             "OO.OOO", // 12
@@ -274,7 +294,7 @@ mod tests {
         ];
 
         for d in Decision::all_valid_decisions() {
-            assert_eq!(is_reachable(&f, d), reachables.contains(d), "d={:?}", d);
+            assert_eq!(pc.is_reachable(&f, d), reachables.contains(d), "d={:?}", d);
         }
     }
 }
--- puyoai-core/src/control/mod.rs
+++ puyoai-core/src/control/mod.rs
@@ -1,6 +1,7 @@
+pub mod controller;
 pub mod key;
 pub mod key_set;
-pub mod reachable;
 
+pub use self::controller::PuyoController;
 pub use self::key::Key;
 pub use self::key_set::KeySet;
